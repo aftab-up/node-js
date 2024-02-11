69f0d8e2503e861cee9f1ecb4c5afb6239399580
aftab-up <aftab.mathproblems@gmail.com>
2024-02-11 23:48:03 +0530

    using pug template

--- routes/shop.js
+++ routes/shop.js
@@ -3,11 +3,18 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin')
 
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  // console.log("SHOP:", adminData.products)
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+
+  // USING PUG
+  const products = adminData.products
+  res.render('shop', {prods : products, title:"Shop", path:'/'})
+
 });
 
 module.exports = router;
