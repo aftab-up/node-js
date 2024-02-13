0eb2cc183b5ce407c94c3b863e9a4dec46c1fc12
aftab-up <aftab.mathproblems@gmail.com>
2024-02-13 22:15:09 +0530

    Enhanced UI

--- models/product.js
+++ models/product.js
@@ -1,35 +1,40 @@
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 
-const rootDir = require("../util/path");
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
 
-const p = path.join(rootDir, "data", "products.json");
-
-const productsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
-    if (!err) {
-      return cb(JSON.parse(fileContent));
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
     }
-    return cb([]);
   });
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    productsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
   }
 
-  // We are defining it as static as we do not want to create class Product everytime . We just want to get the the products
   static fetchAll(cb) {
-    productsFromFile(cb);
+    getProductsFromFile(cb);
   }
 };
